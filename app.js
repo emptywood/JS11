5d8bea132c28eb7340d141a7f6d5a376c809da9a
emptywood <45452376+emptywood@users.noreply.github.com>
2021-06-04 23:20:28 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+$(function() {
+	// let r = 100;
+	let tid;
+	
+	tid = setInterval(dispSecond, 1000);
+	
+	function dispSecond() {
+		let now = new Date();
+		let r = now.getSeconds();
+		let r1 = r % 10;// １の位の数値
+		let r10 = parseInt(r / 10);
+		let singo = 
+		[
+			'1110111',
+			'0000011',
+			'0111110',
+			'0011111',
+			'1001011',
+			'1011101',
+			'1111101',
+			'1010011',
+			'1111111',
+			'1011111'
+		];
+		let stick10 = [$('#s1'), $('#s2'), $('#s3'), $('#s4'), $('#s5'), $('#s6'), $('#s7')];
+		let stick1 = [$('#s11'), $('#s12'), $('#s13'), $('#s14'), $('#s15'), $('#s16'), $('#s17')];
+		
+		// stickの色を元のoffに戻す
+		$('#clock div').css('background-color', '#655e42');
+		
+		for(let i=0; i<7; i++) {
+			if(singo[r10].charAt(i) == 1) {
+				stick10[i].css('background-color', '#150c08');
+			}
+		}
+		for(let i=0; i<7; i++) {
+			if(singo[r1].charAt(i) == 1) {
+				stick1[i].css('background-color', '#150c08');
+			}
+		}
+	}
+});
